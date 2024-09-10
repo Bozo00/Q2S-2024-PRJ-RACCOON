48d984c930add308baf134fe903b38ce8a440fbe
Bozo00 <serban.n.stanescu@gmail.com>
2024-09-11 01:03:41 +0300

    .

--- Code/onGround/on_ground/app/animation/page.tsx
+++ Code/onGround/on_ground/app/animation/page.tsx
@@ -1,4 +1,42 @@
-const Animation3D = () => {
-    
+'use client';
 
+import { Canvas } from "@react-three/fiber";
+
+const Cube = ({pozition, size, color}) => {
+    return (
+        <mesh position={pozition}>
+            <boxGeometry args={size} />
+            <meshStandardMaterial color={color} />
+        </mesh>
+    )
 }
+
+const Animation3D = () => {
+    return (
+        <>
+            <Canvas>
+                <directionalLight position={[0, 0, 2]} />
+
+
+                <Cube pozition={[1, 0, 0]} color="green" size={[1, 1, 1]} />
+
+                <mesh position={[-1, 0, 0]}>
+                    <boxGeometry />
+                    <meshStandardMaterial color={"orange"} />
+                </mesh>
+
+                <mesh position={[-1, 2, 0]}>
+                    <boxGeometry />
+                    <meshStandardMaterial color={"orange"} />
+                </mesh>
+
+                <mesh position={[1, 2, 0]}>
+                    <boxGeometry />
+                    <meshStandardMaterial color={"orange"} />
+                </mesh>
+            </Canvas>
+        </>
+    )
+}
+
+export default Animation3D;
